6732e93711eca62dcb621374305132615a4ad2a6
近空 <87563529+NameLiHua@users.noreply.github.com>
2023-08-24 15:46:58 +0800

    给UserInfo实体类加个builder注解

--- backend/src/main/java/com/wyl/backend/entity/user/UserInfo.java
+++ backend/src/main/java/com/wyl/backend/entity/user/UserInfo.java
@@ -3,17 +3,23 @@ package com.wyl.backend.entity.user;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Builder;
 import lombok.Data;
 
+/**
+ * 用户实体类
+ */
 @TableName("userinfo")
 @Data
+@Builder
 public class UserInfo {
     @TableId
-    private int userid;
+    private Long userid;
     private String username;
     private String password;
     private String registertime;
     private String useremail;
     @TableField(exist = false)
     private String token;
+
 }
