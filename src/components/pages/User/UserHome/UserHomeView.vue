7d31284c6938d1a2844c61b2c21b08a85ca358cd
wyl20020808 <75711101+wyl20020808@users.noreply.github.com>
2023-08-24 17:12:36 +0800

    增加了用户信息界面的部分UI

--- src/components/pages/User/UserHome/UserHomeView.vue
+++ src/components/pages/User/UserHome/UserHomeView.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
+    <div class="all">
         <BaseInfoComponentVue />
     </div>
 </template>
     
 <script>
-import BaseInfoComponentVue from "@/components/component/User/UserHomePage/BaseInfoComponent.vue";
+import BaseInfoComponentVue from "@/components/component/User/UserHomePage/UserInfoCardComponent.vue";
 export default {
     components:{
         BaseInfoComponentVue
@@ -14,5 +14,8 @@ export default {
 </script>
 
 <style scoped>
-
+.all{
+    height: 1000px;;
+    background-color: rgb(180, 207, 234);
+}
 </style>
