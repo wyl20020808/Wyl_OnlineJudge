812ae09b9e0e0719517d446f9caa202cf77cb0c1
近空 <87563529+NameLiHua@users.noreply.github.com>
2023-10-25 16:03:21 +0800

    代码结构修改

--- src/js/functions/config.js
+++
@@ -1,3 +0,0 @@
-export const SERVER_URL = process.env.NODE_ENV === 'production' ? 'http://8.134.48.157:8088' : 'http://localhost:8088';
-export const SERVER = process.env.NODE_ENV === 'production' ? 'http://8.134.48.157' : 'http://localhost';
-export const JUDGE_URL = process.env.NODE_ENV === 'production' ? 'http://8.134.48.157:8088' : 'http://localhost:8088';
